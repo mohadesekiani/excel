180e0b755d3bd080bb6bbe99f892b34863750bd9
kiani <kiani@sps.com>
2024-03-04 17:47:45 +0330

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { ImportExcelFileComponent } from './import-excel-file/import-excel-file.component';
 
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [
     AppComponent,
-    ImportExcelFileComponent
+    ImportExcelFileComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
 MatButtonModule  ],
+exports:[
+  MatButtonModule,
+  MatFormFieldModule,MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/import-excel-file/import-excel-file.component.ts
+++ src/app/import-excel-file/import-excel-file.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import * as Excel from 'exceljs';
 
 @Component({
@@ -6,21 +7,38 @@ import * as Excel from 'exceljs';
   templateUrl: './import-excel-file.component.html',
   styleUrls: ['./import-excel-file.component.css']
 })
-export class ImportExcelFileComponent {
+export class ImportExcelFileComponent implements OnInit {
   headers: string[] = [];
   rows: any[] = [];
-
-
+  form: any
   excelData: any[] = [];
+  constructor(private fb: FormBuilder) { }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(10)]],
+      skills: this.fb.array([])
+
+    });
+  }
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  get skills() {
+    return this.form.get('skills') as FormArray;
+  }
+
+
 
   readExcel(event: any) {
     const workbook = new Excel.Workbook();
     const target: DataTransfer = <DataTransfer>(event.target);
-  
+
     if (target.files.length !== 1) {
       throw new Error('Cannot use multiple files');
     }
-  
+
     const arrayBuffer = new Response(target.files[0]).arrayBuffer();
 
     arrayBuffer.then(data => {
@@ -28,13 +46,19 @@ export class ImportExcelFileComponent {
         const worksheet = workbook.getWorksheet(1);
         if (worksheet) {
           const firstRow = worksheet.getRow(1);
+
           firstRow.eachCell((cell, colNumber) => {
             const cellValue = cell.value;
             if (cellValue) {
+              // this.headers.unshift('rowNumber');
+
               this.headers.push(cellValue.toString());
+
+
             }
+
           });
-        
+          this.headers.unshift("ردیف");
           worksheet.eachRow((row, rowNumber) => {
             if (rowNumber !== 1) { // Skip header row
               const rowData: any[] = [];
@@ -42,6 +66,14 @@ export class ImportExcelFileComponent {
                 rowData.push(cell.value);
               });
               this.excelData.push(rowData);
+              rowData.unshift(rowNumber - 1);
+              const skills = [this.excelData[0][4], this.excelData[0][5], this.excelData[0][6]]
+              this.form.setValue({
+                name: this.excelData[0][1],
+                skills: []
+              })
+              console.log(this.form);
+
             }
           });
         } else {
@@ -50,6 +82,6 @@ export class ImportExcelFileComponent {
       });
     });
   }
-  
-  
+
+
 }
