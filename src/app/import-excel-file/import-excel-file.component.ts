8f85062e03864596bb4827541e0ad839e75b8739
kiani <kiani@sps.com>
2024-03-08 11:36:10 +0330

    change

--- src/app/import-excel-file/import-excel-file.component.ts
+++ src/app/import-excel-file/import-excel-file.component.ts
@@ -12,24 +12,26 @@ export class ImportExcelFileComponent implements OnInit {
   rows: any[] = [];
   form: any
   excelData: any[] = [];
+  visible = false
   constructor(private fb: FormBuilder) { }
 
   buildForm() {
     this.form = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(10)]],
-      skills: this.fb.array([])
-
+      age: ['', [Validators.required, Validators.min(18), Validators.max(99)]],
+      nationalCode: ['', [Validators.required, Validators.minLength(10)]],
+      skills: this.fb.array([
+        this.fb.control(''),
+        this.fb.control(''),
+        this.fb.control('')
+      ],)
     });
   }
   ngOnInit() {
     this.buildForm();
+    console.log(this.form);
+
   }
-
-  get skills() {
-    return this.form.get('skills') as FormArray;
-  }
-
-
 
   readExcel(event: any) {
     const workbook = new Excel.Workbook();
@@ -46,7 +48,7 @@ export class ImportExcelFileComponent implements OnInit {
         const worksheet = workbook.getWorksheet(1);
         if (worksheet) {
           const firstRow = worksheet.getRow(1);
-
+          this.visible = true
           firstRow.eachCell((cell, colNumber) => {
             const cellValue = cell.value;
             if (cellValue) {
@@ -60,18 +62,19 @@ export class ImportExcelFileComponent implements OnInit {
           });
           this.headers.unshift("ردیف");
           worksheet.eachRow((row, rowNumber) => {
-            if (rowNumber !== 1) { // Skip header row
+            if (rowNumber !== 1) {
               const rowData: any[] = [];
               row.eachCell((cell, colNumber) => {
                 rowData.push(cell.value);
               });
               this.excelData.push(rowData);
               rowData.unshift(rowNumber - 1);
-              const skills = [this.excelData[0][4], this.excelData[0][5], this.excelData[0][6]]
-              this.form.setValue({
-                name: this.excelData[0][1],
-                skills: []
-              })
+
+              this.form.get('skills').setValue([this.excelData[0][4], this.excelData[0][5], this.excelData[0][6]]);
+              this.form.get('name').setValue(this.excelData[0][1]);
+              this.form.get('age').setValue(this.excelData[0][2]);
+              this.form.get('nationalCode').setValue(this.excelData[0][3]);
+
               console.log(this.form);
 
             }
